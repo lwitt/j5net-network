3564c84b45d97949eec543ea65c5a138fcdeb005
Laurent WITT <laurent.witt@gmail.com>
2017-11-23 10:05:39 +0100

    initial files

---
+++ j5net-network.cpp
@@ -0,0 +1,139 @@
+#include "Message2.h"
+
+volatile bool adcDone;
+
+// for low-noise/-power ADC readouts, we'll use ADC completion interrupts
+ISR(ADC_vect) { adcDone = true; }
+
+Message::Message(byte source){
+	 	clear();
+		message.source = source;
+}
+
+void Message::clear() {
+	message.header = 0;
+	//message.sequence = 0;
+	payloadSize = 0;
+	memset(message.payload,0,64);
+}
+
+void Message::encode(byte parttype,void* part,byte partsize)
+{
+    if (message.header==0) {
+          message.header=J2NET_HEADER;
+          message.sequence++;
+    }
+
+   message.payload[payloadSize]=parttype;
+   memcpy(&message.payload[payloadSize+1],part,partsize);
+   payloadSize+=partsize+1;
+
+   // for (int i=0;i<cursor;i++){
+ // 	   Serial.print("[");
+ //   		Serial.print(payload[i]);
+ // 		Serial.print("] ");
+ // 	}
+ //   Serial.println();
+}
+
+void Message::decode() {
+	// TO BE DONE
+}
+
+void Message::send(byte destination,byte powermode)
+{
+    if (powermode>0) rf12_sleep(RF12_WAKEUP);
+	int i = 0; while (!rf12_canSend() && i<10) {rf12_recvDone(); i++;}
+
+	// TODO (MEDIUM) : length of header is const (3)
+	rf12_sendStart(RF12_HDR_ACK | RF12_HDR_DST | destination, &message, 3+payloadSize);
+	switch (powermode) {
+		case 0: // standard mode
+			rf12_sendWait(0);
+			break;
+		case 1: // low-power mode
+			rf12_sendWait(2);
+			rf12_sleep(RF12_SLEEP);
+			break;
+		case 2: //ultra low-power mode
+			rf12_sendWait(3);
+			rf12_sleep(RF12_SLEEP);
+			break;
+	}
+
+	clear();
+}
+
+byte Message::getHeader() {
+	return message.header;
+}
+
+byte Message::getSource() {
+	return message.source;
+}
+
+byte Message::getSequence() {
+	return message.sequence;
+}
+
+byte Message::getPayloadByte(byte pos) {
+	return message.payload[pos];
+}
+
+byte Message::getPayloadSize() {
+	return payloadSize;
+}
+
+byte Message::getTotalSize() {
+	return payloadSize+3;
+}
+
+void Message::store(void* data,byte datasize) {
+	memcpy(&message,data,datasize);
+	if (datasize>=3)
+		payloadSize=datasize-3;
+	else
+		payloadSize=0;
+}
+
+void Message::sendSerial() {
+	Serial.print("MSG:");
+	Serial.print(getHeader()); 	Serial.print(' ');
+	Serial.print(getSource());	Serial.print(' ');
+	Serial.print(getSequence());	Serial.print(' ');
+
+	for (byte i = 0; i < getPayloadSize(); i++) {
+		Serial.print(getPayloadByte(i));
+	  	Serial.print(' ');
+  	}
+
+	Serial.println(); Serial.flush(); delay(1);
+}
+
+void Message::saveEncryptionKey(void* key) {
+	//byte numkey[16];
+	//memcpy((void*) key, numkey, 16);
+
+    eeprom_write_block(key, RF12_EEPROM_EKEY, 16);
+    //rf12_encrypt(RF12_EEPROM_EKEY);
+}
+
+byte Message::vccRead (byte count) {
+  set_sleep_mode(SLEEP_MODE_ADC);
+  // use VCC as AREF and internal bandgap as input
+#if defined(__AVR_ATtiny84__)
+  ADMUX = 33;
+#else
+  ADMUX = bit(REFS0) | 14;
+#endif
+  bitSet(ADCSRA, ADIE);
+  while (count-- > 0) {
+    adcDone = false;
+    while (!adcDone)
+      sleep_mode();
+  }
+  bitClear(ADCSRA, ADIE);
+  // convert ADC readings to fit in one byte, i.e. 20 mV steps:
+  //  1.0V = 0, 1.8V = 40, 3.3V = 115, 5.0V = 200, 6.0V = 250
+  return (55U * 1024U) / (ADC + 1) - 50;
+}
---
+++ j5net-network.h
@@ -0,0 +1,105 @@
+#ifndef MESSAGE_h
+#define MESSAGE_h
+
+#include <Arduino.h>
+#include <JeeLib.h>
+#include <avr/sleep.h>
+#include <avr/eeprom.h>
+
+#define J2NET_MASTER_ID 			1
+#define J2NET_NETWORK_ID			77
+
+#define J2NET_HEADER				77
+#define J2NET_HEADER_SIZE			3
+
+#define J2NET_PART_WELCOME			10
+#define J2NET_PART_ALIVE			11
+#define J2NET_PART_CAR_KNOCK			30
+#define J2NET_PART_CAR_QUESTION		31
+#define J2NET_PART_CAR_ANSWER			32
+#define J2NET_PART_TEMP				50
+#define J2NET_PART_PULSE_COUNT		51
+#define J2NET_PART_TEMP_HUMI			52
+#define J2NET_PART_NTP				53
+#define J2NET_PART_RELAY_SET			54
+#define J2NET_PART_RELAY_GET			55
+//#define J2NET_PART_TEMP_HUMI_2		58
+#define J2NET_PART_WEATHER			60
+#define J2NET_PART_WEATHERX			61
+#define J2NET_PART_HOME_STATUS_FAST		65
+#define J2NET_PART_HOME_STATUS_SLOW		66
+#define J2NET_PART_DOOR_BELL			68
+#define J2NET_PART_VCC				70
+
+// multi-part j2net elements
+
+typedef struct{
+	int temperature;
+} PartTemp;
+
+typedef struct{
+	int pulseCount;
+} PartPulseCount;
+
+typedef struct{
+	int temperature;
+	int humidity;
+} PartTempHumi;
+
+typedef struct{
+	byte id;
+	byte value;
+} PartRelay;
+
+typedef struct{
+	byte code;
+} PartDoorBell;
+
+typedef struct{
+	byte vcc;
+} PartVcc;
+
+
+
+
+class Message {
+public:
+	void clear();
+
+	Message(byte source=0);
+
+	void encode(byte parttype,void* part,byte partsize);
+	void decode();
+
+	void send(byte destination,byte powermode);
+
+	byte getHeader();
+	byte getSource();
+	byte getSequence();
+	byte getPayloadByte(byte);
+	byte getPayloadSize();
+	byte getTotalSize();
+
+	void store(void* data,byte datasize);
+
+	void sendSerial();
+
+	void saveEncryptionKey(void* key);
+
+	byte vccRead (byte count =4);
+
+	private:
+	struct {
+	byte header;
+	byte source;
+	byte sequence;
+	byte payload[64];
+	} message;
+
+
+	byte payloadSize;
+};
+
+
+
+#endif
